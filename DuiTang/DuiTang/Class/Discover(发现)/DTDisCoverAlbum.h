45dc53b1f9b82b668ed8506f0f9f0a3edabc4d0c
Monky <617907240@qq.com>
2016-03-14 08:43:07 +0800

    自定义image视图

---
+++ DuiTang/DuiTang/Class/Discover(发现)/DTDisCoverAlbum.h
@@ -0,0 +1,13 @@
+//
+//  DTDisCoverAlbum.h
+//  DuiTang
+//
+//  Created by Monky on 16/3/12.
+//  Copyright © 2016年 Monky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTDisCoverAlbum : UIImageView
+@property (nonatomic,strong)NSString *dId;
+@end
